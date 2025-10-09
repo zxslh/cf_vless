30244bb4ec491390a8b43d8e2ca510f9a9834e8d
zxslh <zxs_lh@msn.com>
2025-10-09 17:17:30 +0800

    Update Dynv6.py

--- Dynv6.py
+++ Dynv6.py
@@ -7,11 +7,13 @@ def update_dynv6_a_via_api(ip, sub_name):
     api_token = os.getenv('DYNV6_TOKEN')
     domain = 'cf-zxs.dns.army'
     ttl = 3600
+    
+    base_url = f"https://dynv6.com/api/v2/zones/5071717/records"
     headers = {
         "Authorization": f"Bearer {api_token}",
         "Content-Type": "application/json"
     }
-    base_url = f"https://dynv6.com/api/v2/zones/5071717/records"
+
     try:
         response = requests.get(base_url, headers=headers)
         response.raise_for_status()
