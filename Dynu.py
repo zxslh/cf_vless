0d4cd01db18ae7b18b698577786be00d6824129a
zxslh <zxs_lh@msn.com>
2025-10-09 15:21:02 +0800

    Update Dynu.py

--- Dynu.py
+++ Dynu.py
@@ -84,7 +84,7 @@ def bulid_vless_urls(a, b):
     uuid = os.getenv('LIVE_CFV_TOKEN')
     port = '443'
     host = 'cfv.live-zxs.dns.army'
-    if not uuid or not host: return
+    if not uuid: return
     vless_url = f"vless://{uuid}@{a}.{b}:{port}?path=%2F%3Fed%3D2560&security=tls&encryption=none&host={host}&type=ws&sni={host}#{a}{b[0]}"
     vless_urls.append(vless_url)
 
