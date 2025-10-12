9d66621b66c82080c06acb7143818e97a1786a73
zxslh <zxs_lh@msn.com>
2025-10-13 00:35:50 +0800

    Update Ddns.py

--- Ddns.py
+++ Ddns.py
@@ -3,7 +3,8 @@ import json
 import re
 import os
 ip_pattern = r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b'
-response = requests.get('https://vps789.com/cfip', timeout=10).text
-ip_matches = re.findall(ip_pattern, response, re.IGNORECASE)()
-print(ip_matches)
+response = requests.get('https://vps789.com/cfip', timeout=10).json()
+print(response)
+#ip_matches = re.findall(ip_pattern, response, re.IGNORECASE)()
+#print(ip_matches)
 
