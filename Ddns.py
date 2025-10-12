71e83ddfa1e1ff6109006f9a7945cbc018d771dc
zxslh <zxs_lh@msn.com>
2025-10-13 01:24:09 +0800

    Update Ddns.py

--- Ddns.py
+++ Ddns.py
@@ -3,7 +3,7 @@ import json
 import re
 import os
 ip_pattern = r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b'
-response = requests.get('https://api.uouin.com/cloudflare.html', timeout=10).text
+response = requests.get('https://api.uouin.com/api/v1/cloudflare/optimize?type=all', timeout=10).text
 ip_matches = re.findall(ip_pattern, response, re.IGNORECASE)
 print(ip_matches)
 
